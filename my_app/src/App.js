c198f160a85421efa33f5790366d82ab39bf7f50
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-27 22:11:36 +0300

    life cycle 3

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HEADER from './Header';
 import HEADER2 from './Header2';
+import HEADER3 from './Header3';
 
 function App() {
   return (
     <div className="App">
       <HEADER favcol="yellow" />
       <HEADER2 favcol="blue" />
+      <HEADER3 favcol="green" />
     </div>
   );
 }
