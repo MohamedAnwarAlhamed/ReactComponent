01a25d716d369d3ae4a97d9061730ec0f1b6aff8
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-27 22:37:26 +0300

    higher order componenet

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import HEADER from './Header';
 import HEADER2 from './Header2';
 import HEADER3 from './Header3';
-
+import CLICKCOUNTER from './components/ClickCounter';
+import HOVERCOUNTER from './components/HoverCounter';
 function App() {
   return (
     <div className="App">
       <HEADER favcol="yellow" />
       <HEADER2 favcol="blue" />
       <HEADER3 favcol="green" />
+###########################################
+      <CLICKCOUNTER />
+      <HOVERCOUNTER />
+
     </div>
   );
 }
